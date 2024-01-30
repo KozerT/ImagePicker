17c2413f0ed42b55840d482433bd21185bf6cee9
KozerT <kozer.vizastaff@gmail.com>
2024-01-30 20:00:28 +0100

    feat: create Modal component to pop up when user selects an image from favorites for deletion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from "react";
 import { AVAILABLE_IMAGES } from "./data.js";
 import Images from "./components/Images.jsx";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation";
 
 function App() {
   const modal = useRef();
@@ -10,6 +12,10 @@ function App() {
   function handleStartRemoveImage(id) {
     modal.current.open();
     selectedImage.current = id;
+  }
+
+  function handleStopRemoveImage() {
+    modal.current.close();
   }
 
   function handleSelectImage(id) {
@@ -22,8 +28,21 @@ function App() {
     });
   }
 
+  function handleRemoveImage() {
+    setPickedImages((prevPickedImages) =>
+      prevPickedImages.filter((image) => image.id !== selectedImage.current)
+    );
+    modal.current.close();
+  }
+
   return (
     <>
+      <Modal ref={modal}>
+        <DeleteConfirmation
+          onCancel={handleStopRemoveImage}
+          onConfirm={handleRemoveImage}
+        />
+      </Modal>
       <header>
         <h1>Dream Destination Gallery</h1>
         <p>
@@ -33,7 +52,7 @@ function App() {
       </header>
       <main>
         <Images
-          title="My curated collection"
+          title="My collection"
           fallbackText={
             "Select images that resonate with you from the gallery."
           }
