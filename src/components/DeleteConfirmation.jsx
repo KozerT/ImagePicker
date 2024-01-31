c6859643d2b45fa2f145fcea270479ac8dce41e5
KozerT <kozer.vizastaff@gmail.com>
2024-01-31 15:09:24 +0100

    feat: Added progress element and its functionality using useEffect, setInterval, and clearInterval functions

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,10 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+const TIMER = 3000;
 
 const DeleteConfirmation = ({ onConfirm, onCancel }) => {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     return () => {
       clearTimeout(timer);
@@ -23,6 +37,7 @@ const DeleteConfirmation = ({ onConfirm, onCancel }) => {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 };
