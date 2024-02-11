ecbc801e98a13515f3cd5622373010b7fa4c59d6
KozerT <kozer.vizastaff@gmail.com>
2024-02-11 23:39:22 +0100

    feat: Implemented image data fetching and display in AvailablePlaces component

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Images from "./Images";
 
-const AvailablePlaces = ({ handleSelectImage }) => {
+const AvailablePlaces = ({ onSelectImage }) => {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/places")
+      .then((response) => response.json())
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
+
   return (
     <Images
       title="Available Places"
-      images={[]}
+      images={availablePlaces}
       fallbackText="No places available."
+      onSelectImage={onSelectImage}
     />
   );
 };
--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-
 const Images = ({ title, images, fallbackText, onSelectImage }) => {
+  console.log(images);
   return (
     <section className="places-category">
       <h2>{title}</h2>
@@ -11,7 +10,7 @@ const Images = ({ title, images, fallbackText, onSelectImage }) => {
             <li key={place.id} className="place-item">
               <button onClick={() => onSelectImage(place)}>
                 <img
-                  src={`http://localhost:3000/${place.image.src}`}
+                  src={`http://localhost:3001/${place.image.src}`}
                   alt={place.image.alt}
                 />
                 <h3>{place.title}</h3>
