6a919fcab6af98fa8d4a276544d597e9ad2bb85c
KozerT <kozer.vizastaff@gmail.com>
2024-02-12 14:31:24 +0100

    fix: Implemented error handling for fetchplaces, displaying Error component with fallback message on data fetch failure.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,21 +1,38 @@
 import { useEffect, useState } from "react";
 import Images from "./Images";
+import Error from "./Error";
 
 const AvailablePlaces = ({ onSelectImage }) => {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     const fetchPlaces = async () => {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3001/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3001/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later",
+        });
+      }
       setIsFetching(false);
     };
     fetchPlaces();
   }, []);
 
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
   return (
     <Images
       title="Available Places"
